e2a60ecc810a3faa0fa5ee5aa0b9ca3bd2d27c96
Pierre V <6772696d@gmail.com>
2024-08-15 10:01:54 +0200

    Complete first part of exercise

---
+++ dom-manipulation/script.js
@@ -0,0 +1,23 @@
+const p = document.createElement("p");
+p.textContent = "Hey I'm red!";
+p.style.color = ("red");
+const body = document.querySelector("body");
+body.appendChild(p)
+
+const h3 = document.createElement("h3");
+h3.textContent = "Hey I'm blue!";
+h3.style.color = "blue";
+body.appendChild(h3);
+
+const div = document.createElement("div");
+div.style.backgroundColor = 'pink';
+div.style.border = 'solid black 1px';
+body.appendChild(div);
+
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div!";
+div.appendChild(h1);
+
+const metoo = document.createElement("p");
+metoo.textContent = "ME TOO!";
+div.appendChild(metoo);
