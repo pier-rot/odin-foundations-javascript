81b2ddfbe8da756d9ea8b2dc43aa3c55bac7d41e
Pierre V <6772696d@gmail.com>
2024-08-15 10:22:46 +0200

    Add 'Events' examples'

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,8 +1,8 @@
 const p = document.createElement("p");
 p.textContent = "Hey I'm red!";
-p.style.color = ("red");
+p.style.color = "red";
 const body = document.querySelector("body");
-body.appendChild(p)
+body.appendChild(p);
 
 const h3 = document.createElement("h3");
 h3.textContent = "Hey I'm blue!";
@@ -10,8 +10,8 @@ h3.style.color = "blue";
 body.appendChild(h3);
 
 const div = document.createElement("div");
-div.style.backgroundColor = 'pink';
-div.style.border = 'solid black 1px';
+div.style.backgroundColor = "pink";
+div.style.border = "solid black 1px";
 body.appendChild(div);
 
 const h1 = document.createElement("h1");
@@ -21,3 +21,13 @@ div.appendChild(h1);
 const metoo = document.createElement("p");
 metoo.textContent = "ME TOO!";
 div.appendChild(metoo);
+
+// Events
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		alert(btn.id);
+	});
+});
